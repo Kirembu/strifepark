b2a44df39bc46b1cdb416fdbf941760c94d059c2
kirembu <in.kirembu@gmail.com>
2012-04-08 20:46:45 +0300

    Cleaning up my repo of Unwanted media

--- static/bootstrap/css/Twitter   Settings_files/direct_messages(1).js
+++
@@ -1,1 +0,0 @@
-"use strict";provide(function(A){using("core/jquery","core/clock","app/data/direct_messages","app/ui/direct_message_dialog",function(F,E,D,C){function B(H){D.attachTo(document,H);C.attachTo("#dm_dialog",H);E.setIntervalEvent("uiNeedsDMConversations",90*1000)}var G=!!F("#dm_dialog").length;A(G?B:F.noop)})});
